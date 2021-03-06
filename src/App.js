d43dd339ad0a5a394596a3c28f4e9ba97b6f00ba
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2021-03-07 00:17:46 +0800

    added particles

--- src/App.js
+++ src/App.js
@@ -3,13 +3,38 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './Components/NavBar';
 import './App.css';
+import Header from './Components/Header';
+import Particles from "react-particles-js";
 
 function App() {
   return (
-    <div>
+    <>
+      <Particles 
       
+       params = {{
+           particles: {
+               number: {
+                   value: 30,
+                   density: {
+                       enable : true,
+                       value_area: 900
+                   }
+               },
+               shape: {
+
+                type: "circle",
+                stroke: {
+
+                    width: 6,
+                    color: "#f9ab00"
+                }
+               }
+           }
+       }}
+      />
       <NavBar />
-    </div>
+      <Header />
+    </>
   )
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+const Header = () => {
+    return (
+        <div className='header-wrapper'>
+            <div className="main-info">
+                <h1> Full-Stack developer and Software tester</h1>
+                <Typed 
+                 className ="typed-text"
+                 strings = { ["Full-stack developer", "Web-designer", "Back-end develper", "Front-end developer"]}
+                 typeSpeed = {40}
+                 backSpeed = {60}
+                 loop
+
+
+                />
+                <a href="" className="btn-main-offer"> Contact Me</a>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
