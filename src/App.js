37cf0a538520c4f04384199382c41740c31ab119
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2021-03-06 15:28:52 +0800

    added basic structure and then added bootstrap file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div>
+      <h2>Dhanyas portfolio</h2>
+    </div>
+  )
+}
+
+export default App
